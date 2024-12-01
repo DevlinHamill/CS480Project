c244e7b65ec997abb49e8a837b42bbd52192dab8
DevlinHamill <devlinswork@yahoo.com>
2024-12-01 04:54:24 -0800

    Updated constants

---
+++ Constants.java
@@ -0,0 +1,26 @@
+package test; 
+
+public class Constants {
+    public static final String STOCK_SYMBOL = "STOCK_SYMBOL";
+    public static final String CURRENT_VALUE = "CURRENT_VALUE";
+    public static final String PREVIOUS_CLOSE = "PREVIOUS_CLOSE";
+    public static final String CHANGE_SINCE_PREVIOUS_CLOSE = "CHANGE_SINCE_PREVIOUS_CLOSE";
+    public static final String CHANGE_SINCE_PREVIOUS_CLOSE_PERCENTAGE = "CHANGE_SINCE_PREVIOUS_CLOSE_PERCENTAGE";
+    public static final String NAME = "Name";
+	public static final String OPEN =  "Open";
+	public static final String BID = "Bid";
+	public static final String ASK = "Ask";
+	public static final String DAYSRANGE = "Days Range"; 
+	public static final String FIFTYTWO_WEEKRANGE = "52 WeekRange";
+	public static final String VOLUME = "Volume";
+	public static final String AVERAGE_VOLUME = "AvgVolume";
+	public static final String MARKETCAP_INTRADAY = "Market Cap intraday";
+	public static final String BETA_5Y_MONTHLY = "Beta 5Y Monthly";
+	public static final String PE_RATION_TTM = "PE Ratio (TTM)";
+	public static final String EPS_TTM = "EPS (TTM)";
+	public static final String EARNINGS_DATE = "Earnings Date";
+	public static final String FORWARD_DIVIDEND_AND_YIELD = "Forward Dividend & Yield";
+	public static final String EX_DIVIDEND_DATE = "Ex Dividend Date";
+	public static final String Y_TARGET_EST = "1y Target Est";
+    
+}
